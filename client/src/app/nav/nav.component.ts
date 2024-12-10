e186d18859f05a478b44946abbdd144b467bd475
Samuel Ridsdale <sam.ridsdale@netpointsolutions.co.uk>
2024-12-10 11:34:26 +0000

    first introduction to angular forms for login

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.scss'
 })
 export class NavComponent {
+
+  model: any = {};
+
+  login() {
+    console.log(this.model);
+  }
 
 }
